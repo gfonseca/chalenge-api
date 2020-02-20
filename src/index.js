0df1430900eb07836eb54ff809c28fb70122d617
Georgio <georgio.barbosa@gmail.com>
2020-02-20 00:24:36 -0300

    Creating server application

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+  res.send('ok')
+})
+
+app.listen(3000)
+console.log('chalenge-api listening on port 3000')
