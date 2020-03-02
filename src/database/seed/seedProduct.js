32eabfae41d6a6918e1f1b0555c06f0ed65c8bb8
Georgio Barbosa <georgio.barbosa@gmail.com>
2020-03-02 13:50:30 -0300

    fix(ssed): bugfix in seed proccess

--- src/database/seed/seedProduct.js
+++ src/database/seed/seedProduct.js
@@ -2,15 +2,14 @@ const getProductModel = require('../../Product/models/Product')
 const fs = require('fs')
 const path = require('path')
 const util = require('util')
-const dbHandler = require('../index')
 
 const mockFile = path.join(__dirname, 'products-mock.json')
 
 const seed = async () => {
-  await dbHandler.clearDatabase()
   const Product = await getProductModel()
   const readFile = util.promisify(fs.readFile)
   const data = await readFile(mockFile)
+  await Product.deleteMany({})
 
   const products = JSON.parse(data)
   Object.keys(products).forEach(async (p, k) => {
